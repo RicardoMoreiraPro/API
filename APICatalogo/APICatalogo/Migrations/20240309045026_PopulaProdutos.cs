a1fdb5ec6686b6daec7aa347e25eeab19503a80c
Ricardo Moreira <123956520+RicardoMoreiraPro@users.noreply.github.com>
2024-03-09 02:02:50 -0300

    Atualização

---
+++ APICatalogo/APICatalogo/Migrations/20240309045026_PopulaProdutos.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+                "VALUES ('Coca cola','Refrigerante de cola 350ml',5.45,'cocacola.jpg',50,getdate(),1)");
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+                "VALUES ('Lanche de Atum','Lanche de Atum com maionese',8.50,'atum.jpg',10,getdate(),2)");
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+                "VALUES ('Pudim','Pudim de leite condensado 100g',6.75,'pudim.jpg',20,getdate(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Produtos");
+        }
+    }
+}
