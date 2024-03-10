c36a491b50ff186fbb08b34e18cb2d7704c8476b
Ricardo Moreira <123956520+RicardoMoreiraPro@users.noreply.github.com>
2024-03-09 22:55:54 -0300

    Atualização

--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc896df68c369d3b94c9a5b773a2772531655b06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1fdb5ec6686b6daec7aa347e25eeab19503a80c")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
